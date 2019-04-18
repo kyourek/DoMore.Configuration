2a772781da833cd9f5b798b9823e52979c713616
Ken Yourek <ken@yourek.net>
2019-04-18 15:45:26 -0500

    (Auto-)Commit version '2.0.0-beta.19'.

--- sln/Domore.Release/ReleaseActions/Push.cs
+++ sln/Domore.Release/ReleaseActions/Push.cs
@@ -2,10 +2,13 @@
 
 namespace Domore.ReleaseActions {
     class Push : ReleaseAction {
+        public string Source { get; set; }
+
         public override void Work() {
             foreach (var pkg in new[] { "Domore.Configuration", "Domore.Configuration.ConfigurationManager" }) {
                 var pkgFile = Path.Combine(SolutionDirectory, $"{pkg}.{Context.Version.StagedVersion}.nupkg");
-                Process("nuget", "push", $"\"{pkgFile}\"", "-Source", "nuget.org");
+                var pkgSource = Source?.Trim() ?? "";
+                Process("nuget", "push", $"\"{pkgFile}\"", "-Source", pkgSource == "" ? "nuget.org" : pkgSource);
             }
         }
     }
