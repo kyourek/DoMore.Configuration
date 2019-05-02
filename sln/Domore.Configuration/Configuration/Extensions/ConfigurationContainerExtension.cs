5f49a1a5f25bb468a7f25ea426e0820ed9cebec8
Ken Yourek <ken@yourek.net>
2019-05-02 08:58:20 -0500

    Organize solution and add release tool.

--- sln/Domore.Configuration/Configuration/Extensions/ConfigurationContainerExtension.cs
+++ sln/Domore.Configuration/Configuration/Extensions/ConfigurationContainerExtension.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+
 namespace Domore.Configuration.Extensions {
     public static class ConfigurationContainerExtension {
         public static T Configure<T>(this IConfigurationContainer configurationContainer, T obj, string key = null) {
