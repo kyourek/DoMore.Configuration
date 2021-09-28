63c3bac4db8abac19b2fbb79e3515835e6243998
Ken Yourek <ken@yourek.net>
2021-09-28 08:51:59 -0500

    Add convenience extension methods.

--- sln/Domore.Conf/Conf/ConfContainerExtension.cs
+++ sln/Domore.Conf/Conf/ConfContainerExtension.cs
@@ -1,14 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Domore.Conf {
     public static class ConfContainerExtension {
-        public static T Configure<T>(this IConfContainer confContainer, T obj, string key = null) {
+        private static IConfBlock ValidateBlock(IConfContainer confContainer) {
             if (null == confContainer) throw new ArgumentNullException(nameof(confContainer));
 
             var block = confContainer.Block;
             if (block == null) throw new ArgumentException(paramName: nameof(confContainer), message: $"{nameof(confContainer.Block)} is null.");
 
-            return block.Configure(obj, key);
+            return block;
+        }
+
+        public static T Configure<T>(this IConfContainer confContainer, T obj, string key = null) {
+            return ValidateBlock(confContainer).Configure(obj, key);
+        }
+
+        public static IEnumerable<T> Configure<T>(this IConfContainer confContainer, Func<T> factory, string key = null, IEqualityComparer<string> comparer = null) {
+            return ValidateBlock(confContainer).Configure(factory, key, comparer);
+        }
+
+        public static IEnumerable<KeyValuePair<string, T>> Configure<T>(this IConfContainer confContainer, Func<string, T> factory, string key = null, IEqualityComparer<string> comparer = null) {
+            return ValidateBlock(confContainer).Configure(factory, key, comparer);
         }
     }
 }
