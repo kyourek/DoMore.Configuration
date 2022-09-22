66fed52d174804c58138a19c4e0cdf05570f38f3
Ken Yourek <ken@yourek.net>
2022-09-22 12:18:16 -0500

    ConfSource needs to be public.

--- sln/Domore.Conf/Conf/Extensions/ConfSource.cs
+++ sln/Domore.Conf/Conf/Extensions/ConfSource.cs
@@ -1,7 +1,7 @@
 ﻿namespace Domore.Conf.Extensions {
     using Text;
 
-    internal static class ConfSource {
+    public static class ConfSource {
         private static TextSourceProvider TextProvider =>
             _TextProvider ?? (
             _TextProvider = new TextSourceProvider());
