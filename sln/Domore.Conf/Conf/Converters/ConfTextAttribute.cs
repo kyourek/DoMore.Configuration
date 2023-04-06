75b9f3de460e8081d3802856cbf0a6e064e51b3e
Ken Yourek <ken@yourek.net>
2023-04-06 12:57:39 -0500

    Configure from inner text.

---
+++ sln/Domore.Conf/Conf/Converters/ConfTextAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Domore.Conf.Converters {
+    using Extensions;
+
+    public sealed class ConfTextAttribute : ConfAttribute {
+        internal sealed override ConfValueConverter ConverterInstance =>
+            _ConverterInstance ?? (
+            _ConverterInstance = new ValueConverter());
+        private ConfValueConverter _ConverterInstance;
+
+        public ConfTextAttribute(params string[] names) : base(names) {
+        }
+
+        private sealed class ValueConverter : ConfValueConverter.Internal {
+            protected sealed override object Convert(bool @internal, string value, ConfValueConverterState state) {
+                if (null == state) throw new ArgumentNullException(nameof(state));
+                var obj = state.Property.GetValue(state.Target, null);
+                if (obj == null) {
+                    obj = Activator.CreateInstance(state.Property.PropertyType);
+                }
+                return obj.ConfFrom(value, key: "");
+            }
+        }
+    }
+}
